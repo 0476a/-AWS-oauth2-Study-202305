a9861c59379ba2bfdf8e3dcb30020532a85bffb8
0476a <0476a@naver.com>
2023-06-25 21:15:17 +0900

    2023.06.25(배포 전 파일 옮기기)

---
+++ oauth2/oauth2-front/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import NotFound from './pages/NotFound/NotFound';
+import Login from './pages/Login/Login';
+import OAuth2Register from './pages/Register/OAuth2Register';
+import OAuth2Merge from './pages/OAuth2Merge/OAuth2Merge';
+import Index from './pages/index/Index';
+import OAuth2Login from './pages/Login/OAuth2Login';
+import AuthRoute from './components/auth/AuthRoute';
+import PostRegister from './pages/Posts/PostRegister';
+import ForgotPassword from './pages/Forgot/ForgotPassword';
+import Profile from './pages/Mypage/Profile';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/auth/forgot/password' element={<AuthRoute path={"/auth/forgot/password"} element={<ForgotPassword />} />} />
+        <Route path='/' element={<AuthRoute path={"/"} element={<Index />} />} />
+
+        <Route path='/mypage/profile' element={<AuthRoute path={"/mypage/profile"} element={<Profile />} />} />
+        <Route path='/post/register' element={<AuthRoute path={"/post/register"} element={<PostRegister />} />} />
+
+        <Route path='/auth/login' element={<AuthRoute path={"/auth/login"} element={<Login />} />} />
+        <Route path='/auth/register' />
+        <Route path='/auth/oauth2/login' element={<AuthRoute path={"/auth/oauth2/login"} element={<OAuth2Login />} />} />
+        <Route path='/auth/oauth2/register' element={<AuthRoute path={"/auth/oauth2/register"} element={<OAuth2Register />} />} />
+        <Route path='/auth/oauth2/merge' element={<AuthRoute path={"/auth/oauth2/merge"} element={<OAuth2Merge />} />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ oauth2/oauth2-front/src/pages/Login/Login.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+    const naviagte = useNavigate();
+
+    const googleAuthClickHandle = () => {
+        window.location.href = `http://localhost:8080/oauth2/authorization/google`;
+    }
+
+    const naverAuthClickHandle = () => {
+        window.location.href = `http://localhost:8080/oauth2/authorization/naver`;
+
+    }
+
+    return (
+        <div>
+            <input type="text" placeholder='email' />
+            <input type="password" placeholder='password' />
+            <button>로그인</button>
+            <button onClick={googleAuthClickHandle}><FcGoogle /></button>
+            <button onClick={naverAuthClickHandle}>네이버</button>
+
+        </div>
+    );
+};
+
+export default Login;
